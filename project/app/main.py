c1044e01862b9c1eb4f6db27a6cdcab079b19fea
Kuds1994 <eduardo.borges13@hotmail.com>
2023-09-14 16:42:53 -0400

    iatecam

--- project/app/main.py
+++ project/app/main.py
@@ -1,29 +1,36 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import List
 
-from app.db import get_session, init_db
-from app.models import Song, SongCreate
+from app.db import get_session
+from app.models import Product, User, Category, ProductWithCategory, UserRead, UserCreate
 
 app = FastAPI()
 
 
-@app.get("/ping")
-async def pong():
-    return {"ping": "pong!"}
+@app.get("/products", response_model=List[ProductWithCategory])
+async def get_products(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Product))
+    products = result.scalars().all()
+    if not products:
+        raise HTTPException(status_code=404, detail="Nenhum produto encontrado")
+    return products
+    ##return [Product(id=product.id, description=product.description, price=product.price, stock=product.stock, category=category) for product, category in products]
 
 
-@app.get("/songs", response_model=list[Song])
-async def get_songs(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Song))
-    songs = result.scalars().all()
-    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
-
-
-@app.post("/songs")
-async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
-    song = Song(name=song.name, artist=song.artist, year=song.year)
-    session.add(song)
+@app.post("/product")
+async def add_song(product: Product, session: AsyncSession = Depends(get_session)):
+    product = Product(description=product.description, price=product.price, stock=product.stock, category_id=product.category_id)
+    session.add(product)
     await session.commit()
-    await session.refresh(song)
-    return song
+    await session.refresh(product)
+    return product
+
+@app.post("/user", response_model=UserRead)
+async def add_song(user: UserCreate, session: AsyncSession = Depends(get_session)):
+    user = User.from_orm(user)
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
--- project/app/models.py
+++ project/app/models.py
@@ -1,16 +1,48 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List
 
-
-class SongBase(SQLModel):
+class CategoryBase(SQLModel):
     name: str
-    artist: str
-    year: Optional[int] = None
 
+class Category(CategoryBase, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+    products: List["Product"] = Relationship(back_populates="category", sa_relationship_kwargs={'lazy': 'selectin'})
 
-class Song(SongBase, table=True):
+class CategoryRead(CategoryBase):
+    id: int
+
+class ProductBase(SQLModel):
+    description: str
+    price: float
+    stock: int
+
+class Product(ProductBase, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+    category_id: int = Field(default=None, nullable=False, foreign_key='category.id')
+    category: Category = Relationship(back_populates='products', sa_relationship_kwargs={'lazy': 'selectin'})
+
+class ProductRead(ProductBase):
+    id: int    
+
+class ProductWithCategory(ProductRead):
+    category: CategoryRead = None
+
+class CategoryWithProducts(CategoryRead):
+    products: List[ProductRead] = []
+
+class UserBase(SQLModel):
+    name: str
+    username: str
+    password: str
+
+class User(UserBase, table=True):
     id: int = Field(default=None, nullable=False, primary_key=True)
 
+class UserRead(UserBase):
+    id: int
 
-class SongCreate(SongBase):
+class UserCreate(UserBase):
     pass
+
+
+    
